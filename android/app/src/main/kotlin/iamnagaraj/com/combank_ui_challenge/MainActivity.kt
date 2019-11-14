1326f95227c17ad2dc9f77e0fdf0a09934687c19
Nagaraj Alagusundaram <nag@xam-consulting.com>
2019-11-14 16:37:49 +1100

    Initial commit

---
+++ android/app/src/main/kotlin/iamnagaraj/com/combank_ui_challenge/MainActivity.kt
@@ -0,0 +1,12 @@
+package iamnagaraj.com.combank_ui_challenge
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
